2a2797e34609368828221856bfa61a4e23716bdb
OmarMesqq <omarmsqt@gmail.com>
2023-12-16 20:57:49 -0300

    feat: most wc functions finished

--- coding_challenges/ccwc/main.lua
+++ coding_challenges/ccwc/main.lua
@@ -1,5 +1,5 @@
 for i = 1, #arg do
-    if arg[i] == "-c" then -- OK
+    if arg[i] == "-c" then
         local f = assert(io.open(arg[i + 1], "rb"))
         local count = 0
         while true do
@@ -9,7 +9,7 @@ for i = 1, #arg do
         end
         f:close()
         print(count, arg[i + 1])
-    elseif arg[i] == "-l" then -- OK
+    elseif arg[i] == "-l" then
         local f = assert(io.open(arg[i + 1], "rb"))
         local count = 0
         for line in f:lines() do
@@ -18,7 +18,7 @@ for i = 1, #arg do
         f:close()
         print(count, arg[i + 1])
 
-    elseif arg[i] == "-w" then -- OK
+    elseif arg[i] == "-w" then
         local f = assert(io.open(arg[i + 1], "rb"))
         local count = 0
         for line in f:lines() do
@@ -31,20 +31,20 @@ for i = 1, #arg do
     elseif arg[i] == "-m" then
         local f = assert(io.open(arg[i + 1], "rb"))
         local count = 0
-        local line = f:read("*l")
+        local line = f:read("*l") -- reads line without newline
 
         while line do
-            count = count + string.len(line)  -- Count characters in the line
+            count = count + string.len(line)  
             local nextLine = f:read("*l")
             if nextLine then
-                count = count + 1  -- Add 1 for the newline character
+                count = count + 1
             end
             line = nextLine
         end
 
         print(count, arg[i + 1])
         f:close()
-    
+    else
+        
     end
-    
 end
