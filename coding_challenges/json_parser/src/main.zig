92283ba7137b072eda678413191c73012f97c506
OmarMesqq <omarmsqt@gmail.com>
2023-12-18 02:00:02 -0300

    refactor(codingChallenges): json_parser: print file content to stdout

--- coding_challenges/json_parser/src/main.zig
+++ coding_challenges/json_parser/src/main.zig
@@ -2,6 +2,34 @@ const std = @import("std");
 const Lexer = @import("lexer.zig").Lexer;
 const Parser = @import("parser.zig").Parser;
 
-pub fn main() void {
-    // Implement
+const AppErrors = error{InsufficientArgumentsError};
+
+pub fn main() !void {
+    const args = try std.process.argsAlloc(std.heap.page_allocator);
+    defer std.heap.page_allocator.free(args);
+
+    if (args.len < 2) {
+        std.debug.print("Usage: {s} <filename>\n", .{args[0]});
+        return error.InsufficientArguments;
+    }
+
+    const filename = args[1];
+    std.debug.print("Filename: {s}\n", .{filename});
+
+    const file = try std.fs.cwd().openFile(filename, .{
+        .mode = .read_only, // create an anonymous struct on the fly
+    });
+    defer file.close();
+
+    const file_stat = try file.stat();
+    const file_size = file_stat.size;
+
+    var buffer = try std.heap.page_allocator.alloc(u8, file_size);
+    defer std.heap.page_allocator.free(buffer);
+
+    const reader = file.reader();
+    _ = try reader.read(buffer);
+
+    const fileContents = std.mem.sliceTo(buffer, 0); // search the buffer until null terminator
+    std.debug.print("File Contents: {s}\n", .{fileContents});
 }
